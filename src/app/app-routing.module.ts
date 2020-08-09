568144b1567f81e6026e48da58b4add253f27aa5
apurba <apurbakumar.3110@gmail.com>
2020-08-09 15:40:57 +0530

    Adding routing for the componenets in app-component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'search-customers', component: SearchCustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateCustomerComponent } from './create-customer/create-customer.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
+import { SearchCustomersComponent } from './search-customers/search-customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCustomerComponent,
+    CustomerDetailsComponent,
+    CustomersListComponent,
+    SearchCustomersComponent
   ],
   imports: [
     BrowserModule,
